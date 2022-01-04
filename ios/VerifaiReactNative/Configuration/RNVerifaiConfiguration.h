5bd3b715c4239c54731c43348abf6e48e9d40e58
Richard Chirino <gitkraken@giantrobot.nl>
2022-01-04 15:00:50 +0100

    somewhat running version of a react native app to test stuff on

---
+++ ios/VerifaiReactNative/Configuration/RNVerifaiConfiguration.h
@@ -0,0 +1,17 @@
+//
+//  RNVerifaiConfiguration.h
+//  VerifaiReactNative
+//
+//  Created by Richard Chirino on 03/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNVerifaiConfiguration : NSObject <RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/VerifaiReactNative/Configuration/RNVerifaiConfiguration.m
@@ -0,0 +1,16 @@
+//
+//  RNVerifaiConfiguration.m
+//  VerifaiReactNative
+//
+//  Created by Richard Chirino on 03/01/2022.
+//
+
+#import "RNVerifaiConfiguration.h"
+
+@implementation RNVerifaiConfiguration
+
++ (NSString *)moduleName {
+  return @"RNVerifaiConfiguration";
+}
+
+@end
